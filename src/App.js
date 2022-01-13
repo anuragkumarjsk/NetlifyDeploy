a63cd69a38b9a88ede1bfa4814ac4b510ffbbff4
anuragkumarjsk <anuragkumarjsk13@gmai.com>
2022-01-13 13:47:50 +0530

    deployed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Route,Routes,Link} from 'react-router-dom'
+import {BrowserRouter as Router, Route,Routes} from 'react-router-dom'
 import Home from './Components/Home/Home';
 import About from './Components/About/About';
 import Contact from './Components/Contact/Contact';
